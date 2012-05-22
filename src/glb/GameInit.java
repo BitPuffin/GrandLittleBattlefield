cbebd4cb4bb5b93641978526c987e41127544508
Isak Andersson <BitPuggin@lavabit.com>
2012-05-22 10:41:37 +0200

    got that engine

--- src/glb/GameInit.java
+++ src/glb/GameInit.java
@@ -4,13 +4,24 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 //	==========================================
 //	=		This is what most people call
 //	=		the "main" class!
 //	==========================================
 
-public class GameInit extends {
+public class GameInit extends StateBasedGame {
+	
+	public GameInit( String windowFackingCuntTitleDontDenyItICanPromiseThatItWillShowUpOnTheGameWindowUnlessItIsFullScreenSoThereYouHaveIt ) {
+		super( windowFackingCuntTitleDontDenyItICanPromiseThatItWillShowUpOnTheGameWindowUnlessItIsFullScreenSoThereYouHaveIt );
+	}
+	
+	@Override
+	public
+	void initStatesList( GameContainer container ) {
+		addState( new War( 1, container ) );
+	}
 	
 	//	Some basic fields required for the game
 	private static final String		gameName	= "Grand Little Battlefield";
